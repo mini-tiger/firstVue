6a2aab04396892d30a98d868d736bdb009d39478
taojun <61566027@163.com>
2021-04-23 17:08:39 +0800

    first commit

---
+++ src/config/routes.js
@@ -0,0 +1,60 @@
+import HeaderAsideLayout from '@/layouts/HeaderAsideLayout';
+import NotFound from '@/pages/NotFound';
+import Dashboard from '@/pages/Dashboard';
+import TabTable  from '@/pages/TableDemo/TabTable'
+
+const routerConfig = [
+  {
+    path: '/table',
+    component: HeaderAsideLayout,
+    children: [
+      { path: '/table/basic', component: TabTable },
+      { path: '/table/fixed', component: NotFound },
+    ],
+  },
+  {
+    path: '/form',
+    component: HeaderAsideLayout,
+    children: [
+      { path: '/form/basic', component: NotFound },
+      { path: '/form/signup', component: NotFound },
+    ],
+  },
+  {
+    path: '/charts',
+    component: HeaderAsideLayout,
+    children: [
+      { path: '/charts/line', component: NotFound },
+      { path: '/charts/histogram', component: NotFound },
+      { path: '/charts/bar', component: NotFound },
+    ],
+  },
+  {
+    path: '/profile',
+    component: HeaderAsideLayout,
+    children: [
+      { path: '/profile/success', component: NotFound },
+      { path: '/profile/fail', component: NotFound },
+    ],
+  },
+  {
+    path: '/result',
+    component: HeaderAsideLayout,
+    children: [
+      { path: '/result/success', component: NotFound },
+      { path: '/result/fail', component: NotFound },
+    ],
+  },
+  {
+    path: '/',
+    component: HeaderAsideLayout,
+    children: [
+      { path: '/dashboard/analysis', component: Dashboard },
+      { path: '/dashboard/monitor', component: NotFound },
+      { path: '/dashboard/workplace', component: NotFound },
+    ],
+  },
+  { path: '*', component: NotFound },
+];
+
+export default routerConfig;
